6e9f25165a4a1d6f90c4c8ea0b1e0c324da19e78
fernandofigueredo-cyber <fernandofigueredo313@gmail.com>
2026-09-03 17:52:07 -0300

    Update SettingsPanel.tsx

--- src/components/SettingsPanel.tsx
+++ src/components/SettingsPanel.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ParentalSettings } from './parental/ParentalSettings';
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Check, Eraser, Volume2 } from "lucide-react";
